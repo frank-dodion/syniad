af2ce2e765e85fe1f50b6746944e925a3b67a18f
Frank Phillips <frank@dodion.co.uk>
2025-11-07 00:26:36 +0000

    Updated versions

--- terraform/nextjs-lambda.tf
+++ terraform/nextjs-lambda.tf
@@ -1,8 +1,9 @@
 # AWS Lambda Web Adapter Layer
-# Using the official AWS Lambda Web Adapter layer
-data "aws_lambda_layer_version" "web_adapter" {
-  layer_name = "AWSLambdaWebAdapter"
-  version     = 2  # Use latest version
+# Using the official AWS Lambda Web Adapter layer (X86_64 architecture)
+# Layer ARN format: arn:aws:lambda:${region}:753240598075:layer:LambdaAdapterLayerX86:${version}
+# Version 21 is a recent stable version (as of 2024)
+locals {
+  web_adapter_layer_arn = "arn:aws:lambda:${var.aws_region}:753240598075:layer:LambdaAdapterLayerX86:21"
 }
 
 # Data source for Next.js scenario-editor package
@@ -52,7 +53,7 @@ resource "aws_lambda_function" "scenario_editor" {
   memory_size     = 512
   source_code_hash = data.archive_file.scenario_editor_lambda.output_base64sha256
 
-  layers = [data.aws_lambda_layer_version.web_adapter.arn]
+  layers = [local.web_adapter_layer_arn]
 
   environment {
     variables = {
@@ -61,9 +62,10 @@ resource "aws_lambda_function" "scenario_editor" {
       NEXT_PUBLIC_FRONTEND_URL = "https://${local.editor_domain_name}"
       AWS_LAMBDA_EXEC_WRAPPER = "/opt/bootstrap"
       NEXTAUTH_URL            = "https://${local.editor_domain_name}"
-      NEXTAUTH_SECRET         = "change-me-in-production" # TODO: Use secrets manager
+      BETTER_AUTH_SECRET      = "change-me-in-production-min-32-chars" # TODO: Use secrets manager
       COGNITO_USER_POOL_ID    = aws_cognito_user_pool.users.id
       COGNITO_CLIENT_ID       = aws_cognito_user_pool_client.web_client.id
+      COGNITO_CLIENT_SECRET   = "" # Public client, no secret needed
       COGNITO_REGION          = var.aws_region
       COGNITO_DOMAIN          = "${aws_cognito_user_pool_domain.auth_domain.domain}.auth.${var.aws_region}.amazoncognito.com"
     }
@@ -83,7 +85,7 @@ resource "aws_lambda_function" "game" {
   memory_size     = 512
   source_code_hash = data.archive_file.game_lambda.output_base64sha256
 
-  layers = [data.aws_lambda_layer_version.web_adapter.arn]
+  layers = [local.web_adapter_layer_arn]
 
   environment {
     variables = {
